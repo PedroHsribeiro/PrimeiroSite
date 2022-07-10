a194986543cd5a9a628f7341582e3dc505f8fd36
Pedro <pedro.hsribeiro@hotmail.com>
2022-07-10 00:34:25 -0300

    implantação javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 function mostrarMenu(){
-    
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == "none"){
+        menu.style.display = "flex"
+    } else{
+        menu.style.display = "none"
+    }
+}
+function mostrarPesquisa(){
+    let pesquisa = document.getElementById("search-box")
+
+    if(getComputedStyle(pesquisa).display == "none"){
+        pesquisa.style.display = "flex"
+    } else{
+        pesquisa.style.display = "none"
+    }
 }

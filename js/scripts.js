62aa2edd8d6ee57a49113f465b46fb34564f054e
Pedro <pedro.hsribeiro@hotmail.com>
2022-07-12 22:58:06 -0300

    javascript footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function mostrarMenu(){
+function mostrarMenu(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
     let menu = document.getElementById("menu")
 
     if (getComputedStyle(menu).display == "none"){
@@ -7,7 +7,7 @@ function mostrarMenu(){
         menu.style.display = "none"
     }
 }
-function mostrarPesquisa(){
+function mostrarPesquisa(){ //Este código é utilizado para apresentar a barra de pesquisa em displays menores que 1024px
     let pesquisa = document.getElementById("search-box")
 
     if(getComputedStyle(pesquisa).display == "none"){
@@ -16,3 +16,42 @@ function mostrarPesquisa(){
         pesquisa.style.display = "none"
     }
 }
+function mostrarFooter(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
+    let footerItems = document.getElementById("inst2")
+
+    if (getComputedStyle(footerItems).display == "none"){
+        footerItems.style.display = "flex"
+    } else{
+        footerItems.style.display = "none"
+    }
+}
+function mostrarFooter1(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
+    let footerItems = document.getElementById("depart2")
+
+    if (getComputedStyle(footerItems).display == "none"){
+        footerItems.style.display = "flex"
+    } else{
+        footerItems.style.display = "none"
+    }
+}
+function mostrarFooter2(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
+    let footerItems = document.getElementById("accou2")
+
+    if (getComputedStyle(footerItems).display == "none"){
+        footerItems.style.display = "flex"
+    } else{
+        footerItems.style.display = "none"
+    }
+}
+function mostrarFooter3(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
+    let footerItems = document.getElementById("atend2")
+
+    if (getComputedStyle(footerItems).display == "none"){
+        footerItems.style.display = "flex"
+    } else{
+        footerItems.style.display = "none"
+    }
+}
+
+
+

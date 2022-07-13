aaa8d429118456304895685ff603e78236a5bc9e
Pedro <pedro.hsribeiro@hotmail.com>
2022-07-12 23:25:55 -0300

    javascript footer

--- js/scripts.js
+++ js/scripts.js
@@ -18,38 +18,46 @@ function mostrarPesquisa(){ //Este código é utilizado para apresentar a barra 
 }
 function mostrarFooter(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
     let footerItems = document.getElementById("inst2")
-
-    if (getComputedStyle(footerItems).display == "none"){
-        footerItems.style.display = "flex"
-    } else{
-        footerItems.style.display = "none"
+    let tamanhoTela = window.innerWidth;
+    if(tamanhoTela < 830){
+        if (getComputedStyle(footerItems).display == "none"){
+            footerItems.style.display = "flex"
+        } else{
+            footerItems.style.display = "none"
+        }
     }
 }
 function mostrarFooter1(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
     let footerItems = document.getElementById("depart2")
-
-    if (getComputedStyle(footerItems).display == "none"){
-        footerItems.style.display = "flex"
-    } else{
-        footerItems.style.display = "none"
+    let tamanhoTela = window.innerWidth;
+    if(tamanhoTela < 830){
+        if (getComputedStyle(footerItems).display == "none"){
+            footerItems.style.display = "flex"
+        } else{
+            footerItems.style.display = "none"
+        }
     }
 }
 function mostrarFooter2(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
     let footerItems = document.getElementById("accou2")
-
-    if (getComputedStyle(footerItems).display == "none"){
-        footerItems.style.display = "flex"
-    } else{
-        footerItems.style.display = "none"
+    let tamanhoTela = window.innerWidth;
+    if(tamanhoTela < 830){
+        if (getComputedStyle(footerItems).display == "none"){
+            footerItems.style.display = "flex"
+        } else{
+            footerItems.style.display = "none"
+        }
     }
 }
 function mostrarFooter3(){ //Este código é utilizado para apresentar os itens do menu em displays menores que 1024px
     let footerItems = document.getElementById("atend2")
-
-    if (getComputedStyle(footerItems).display == "none"){
-        footerItems.style.display = "flex"
-    } else{
-        footerItems.style.display = "none"
+    let tamanhoTela = window.innerWidth;
+    if(tamanhoTela < 830){
+        if (getComputedStyle(footerItems).display == "none"){
+            footerItems.style.display = "flex"
+        } else{
+            footerItems.style.display = "none"
+        }
     }
 }
 
